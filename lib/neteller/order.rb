b02b6de29ce6833b813a835b46840bcb0022fe6c
Jasbir Singh <jasbirsingh279@gmail.com>
2015-09-10 13:51:44 +0100

    Add method to render payment in the format required by neteller

---
+++ lib/neteller/order.rb
@@ -0,0 +1,30 @@
+module Neteller
+  class Order
+    def initialize(options={})
+      options.each { |name, value| instance_variable_set("@#{name}", value) }
+    end
+
+    def to_h
+      order = {
+        "merchantRefId": @merchant_ref_id,
+        "totalAmount": @total_amount,
+        "currency": @currency,
+        "lang": @lang,
+        "items": [{"name": @item_name}],
+        "redirects": [
+          {
+            "rel": "on_success",
+            "returnKeys": [],
+            "uri": @redirects_success
+          },
+          {
+            "rel": "on_cancel",
+            "returnKeys": [],
+            "uri": @redirects_cancel
+          }
+        ]
+      }
+      { order: order }
+    end
+  end
+end
--- spec/neteller_spec.rb
+++ spec/neteller_spec.rb
@@ -5,36 +5,37 @@ describe "Order" do
   let (:order) do
     Neteller::Order.new({
       merchant_ref_id: '123123',
-      total_amount: '3599',
+      total_amount: 3599,
       currency: 'EUR',
       lang: 'en_US',
       ip: '123.123.123',
-      redirects_success: 'yes...',
-      redirects_cancel: 'no...',
+      redirects_success: "https://example.com/success.html",
+      redirects_cancel: "https://example.com/cancel.html",
       item_name: '100 Iconic Coins'
     })
   end
 
   let (:order_params) do
-    {
+    order = {
       "merchantRefId": "123123",
       "totalAmount": 3599,
       "currency": "EUR",
       "lang": "en_US",
-      "items": [{"name": "Item A"}],
+      "items": [{"name": "100 Iconic Coins"}],
       "redirects": [
         {
           "rel": "on_success",
-          "returnKeys": ["id"],
+          "returnKeys": [],
           "uri": "https://example.com/success.html"
         },
         {
           "rel": "on_cancel",
-          "returnKeys": ["id"],
+          "returnKeys": [],
           "uri": "https://example.com/cancel.html"
         }
       ]
     }
+    { order: order }
   end
 
   it "renders an Order in JSON format required by Neteller" do
