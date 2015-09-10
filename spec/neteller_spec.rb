c55d4a27cee70d6cd3eee81c114c2580dc8158a0
Michael Worthingon <ppyxmw@gmail.com>
2015-09-10 12:07:39 +0100

    adds order spec

--- spec/neteller_spec.rb
+++ spec/neteller_spec.rb
@@ -1,7 +1,49 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Neteller" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Order" do
+
+  let (:order) do
+    Neteller::Order.new({
+      merchant_ref_id: '123123',
+      total_amount: '3599',
+      currency: 'EUR',
+      lang: 'en_US',
+      ip: '123.123.123',
+      redirects_success: 'yes...',
+      redirects_cancel: 'no...'
+      item_name: '100 Iconic Coins'
+    })
+  end
+
+  let (:order_params) do
+
+   {
+     "merchantRefId": "123123",
+     "totalAmount": 3599,
+     "currency": "EUR",
+     "lang": "en_US",
+     "items": [{"name": "Item A"}],
+     "redirects": [
+      {
+       "rel": "on_success",
+       "returnKeys": [
+         "id"
+         ],
+         "uri": "https://example.com/success.html"
+         },
+         {
+           "rel": "on_cancel",
+           "returnKeys": [
+             "id"
+             ],
+             "uri": "https://example.com/cancel.html"
+          }
+        ]
+    }
+  end
+
+  it "renders an Order in JSON format required by Neteller" do
+    expect(order.to_h) eq(order_params)
   end
 end
+
