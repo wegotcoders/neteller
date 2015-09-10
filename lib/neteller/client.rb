d5c8b0c97aee454b5b940fc29017fcb740632718
Jasbir Singh <jasbirsingh279@gmail.com>
2015-09-10 17:44:55 +0100

    Add method to obtain access token before each request

--- lib/neteller/client.rb
+++ lib/neteller/client.rb
@@ -1,3 +1,5 @@
+require 'base64'
+
 module Neteller
   class Client
     include HTTParty
@@ -11,15 +13,19 @@ module Neteller
     end
 
     def initialize
-      @client_id, @secret_id, @merchant_ref_id = @@config.to_a
+      @client_id, @client_secret, @merchant_ref_id = @@config.to_a
     end
 
     def pay!(payment)
       headers = {
         "Content-Type" => 'application/json',
-        "Authorization" => 'ACCESS_TOKEN'
+        "Authorization" => "Bearer #{obtain_access_token}"
        }
       HTTParty.post("https://api.neteller.com/v1/orders", :body => payment.to_h.to_json, :headers => headers )
+    end
+
+    def obtain_access_token
+      HTTParty.post("https://api.neteller.com/v1/oauth2/token?grant_type=client_credentials", :basic_auth => Base64.encode("#{@client_id}:#{@client_secret}"))
     end
   end
 end
