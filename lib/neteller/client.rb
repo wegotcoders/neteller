b3fdc0789ed907b234b9187a10ed231a4939555b
Jasbir Singh <jasbirsingh279@gmail.com>
2015-09-20 18:12:17 +0100

    Refractor Client class

--- lib/neteller/client.rb
+++ lib/neteller/client.rb
@@ -14,24 +14,25 @@ module Neteller
       @client_id, @client_secret, @webhook_key = @@config.to_a
     end
 
-    def pay!(payment)
-      headers = {
+    def headers
+      {
         "Content-Type" => "application/json",
         "Authorization" => "Bearer #{obtain_access_token['accessToken']}"
       }
+    end
+
+    def pay!(payment)
       response = HTTParty.post("https://test.api.neteller.com/v1/orders", :body => payment.to_h.to_json, :headers => headers )
       Response.new(response).to_h
     end
 
     def obtain_access_token
-      HTTParty.post("https://test.api.neteller.com/v1/oauth2/token?grant_type=client_credentials", :headers => {'Content-Type' => 'application/json', 'Cache-control' => 'no-cache' }, :basic_auth => { username: @client_id, password: @client_secret})
+      HTTParty.post("https://test.api.neteller.com/v1/oauth2/token?grant_type=client_credentials",
+                    :headers => { 'Content-Type' => 'application/json', 'Cache-control' => 'no-cache' },
+                    :basic_auth => { username: @client_id, password: @client_secret })
     end
 
     def order_lookup(url)
-      headers = {
-        "Content-Type" => "application/json",
-        "Authorization" => "Bearer #{obtain_access_token['accessToken']}"
-      }
       HTTParty.get(url[0..-9], :headers => headers).to_h
     end
   end
