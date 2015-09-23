80d75575dcd68a6381adcffe5339e98aff72c816
Jasbir Singh <jasbirsingh279@gmail.com>
2015-09-23 11:30:47 +0100

    Implement live mode

--- lib/neteller/config.rb
+++ lib/neteller/config.rb
@@ -1,6 +1,6 @@
 module Neteller
   class Config
-    attr_accessor :client_id, :client_secret, :merchant_ref_id, :webhook_key
+    attr_accessor :client_id, :client_secret, :merchant_ref_id, :webhook_key, :live_mode
 
     def initialize
       yield self
@@ -8,7 +8,16 @@ module Neteller
     end
 
     def to_a
-      [@client_id, @client_secret, @webhook_key]
+      [@client_id, @client_secret, @webhook_key, base_uri]
+    end
+
+    def base_uri
+      "https://#{subdomain}api.neteller.com"
+    end
+
+    private
+    def subdomain
+      self.live_mode ? '' : 'test.'
     end
   end
 end
